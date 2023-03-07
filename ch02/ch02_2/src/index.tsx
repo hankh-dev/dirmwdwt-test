454ed3c5cbda2e9357725c81b96e533aecac889b
hankh <hankh.dev@gmail.com>
2023-03-07 13:22:11 +0900

    chore

--- ch02/ch02_2/src/index.tsx
+++ ch02/ch02_2/src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const children = [
+  <li>
+    <a href="http://www.google.com" target="_blank">
+      <p>go to Google</p>
+    </a>
+  </li>,
+  <li>
+    <a href="http://www.facebook.com" target="_blank">
+      <p>go to Facebook</p>
+    </a>
+  </li>,
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootVirtualDOM = <ul>{children}</ul>
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(rootVirtualDOM)
